a2c1a53f364c707f96caeba1e68dc21fa1b51217
Carl Lange <carl@flax.ie>
2011-12-29 10:12:45 +0000

    How did I forget the actual speech support bit?

---
+++ gspeech.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3
+
+import urllib.request,urllib.parse,urllib.error 
+import socket
+import json
+import subprocess
+
+def send(filestr):
+	with open(filestr, 'rb') as file:
+		audio = file.read()
+		try:
+			#audio = urllib.parse.urlencode(audio)
+			url = urllib.request.Request("https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US", audio)
+			url.add_header("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.29 Safari/525.13")
+			url.add_header("Content-Type", "audio/x-flac; rate=16000")
+			resp = urllib.request.urlopen(url).read().decode("utf8", 'ignore')
+			print(resp)
+			return json.loads(resp)["hypotheses"][0]["utterance"]
+
+		except urllib.error.HTTPError as e:
+			print(e.read().decode("utf8", 'ignore'))
+		except urllib.error.URLError:
+			print("URLError")
+		except socket.error:
+			print("socket.error")
+		except socket.timeout:
+			print("socket.timeout")
+
+def hear():
+	subprocess.call("rec -r 16000 -c 1 out.flac silence 0 1 0:00:03 2%", shell=True)
+	return("out.flac")
