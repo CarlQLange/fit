1cac280484611779b00779e246fcb5613034bfe3
Carl Lange <carl@flax.ie>
2011-12-30 16:36:41 +0000

    Improved the page the server serves up a little.

--- web/server.py
+++ web/server.py
@@ -6,9 +6,9 @@ class posthandler(SimpleHTTPRequestHandler):
 	def do_POST(self):
 		length = int(self.headers['Content-Length'])
 		post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-		print(post_data["inp"][0])
+		print(post_data)
 		os.system('python3 ../fitunes.py "%s"' % post_data["inp"][0])
-		self.send_response(301)
+		self.send_response(200)
 
 httpd = HTTPServer(('127.0.0.1', 5171), posthandler)
 httpd.serve_forever()
