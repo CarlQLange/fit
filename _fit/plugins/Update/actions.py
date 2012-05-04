3a54ae9207a5ab61ffb1bec3e08cb768ff1596b3
Carl Lange <carl@flax.ie>
2012-05-04 13:45:00 -0700

    Whitespace change

--- _fit/plugins/Update/actions.py
+++ _fit/plugins/Update/actions.py
@@ -2,8 +2,8 @@ import os
 
 def gitupdate():
 	os.system("""
-		mkdir ~/bin/.fitupdate 
-	 && git clone -b git://github.com/CarlQLange/fit.git master 
+		mkdir ~/bin/.fitupdate
+	 && git clone -b git://github.com/CarlQLange/fit.git master
 	 && ./install.sh
 	 && rm -r ~/bin/.fitupdate
 	""")
